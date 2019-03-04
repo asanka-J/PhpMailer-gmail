f9eb5c0df63bf5991e160e2263dbad33f3a17c4e
asanka94 <asanka.jsndr@gmail.com>
2019-03-04 18:42:26 +0530

    Init repo

---
+++ index.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="sendemail.php" method="post">
+Name: <input type="text" name="name"><br>
+E-mail: <input type="text" name="email"><br>
+Subject: <input type="text" name="subject"><br>
+message: <input type="text" name="message"><br>
+
+<input type="submit" name="submit">
+</form>
+
+</body>
+</html>
